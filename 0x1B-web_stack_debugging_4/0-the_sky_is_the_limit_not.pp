29935233a4c6b3a9ca65d37b7302ee1044e32624
obuong <iobuong@gmail.com>
2023-05-18 21:54:18 +0300

    make 2000 requests to my server with 100 requests at a time

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This handles requests of high volume on the server
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
